0dd173973d76e5210e0f3eb53c712effef7749da
Adi Sneh <adisneh@gmail.com>
2020-10-04 15:01:16 +0300

    Add tv frame and white screen underneath it.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import tv from './tv.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <div className="Tv">
+                <div className="Screen">
+                    Sup
+                </div>
+                <img src={tv} alt="tv frame" className="Tv-frame"/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
